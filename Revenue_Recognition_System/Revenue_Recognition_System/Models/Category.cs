5eb33cba895006cb870a5558dd6a1cb0513b21f6
fz3002 <franc3002@gmail.com>
2024-06-23 17:29:04 +0200

    Category and Software model

---
+++ Revenue_Recognition_System/Revenue_Recognition_System/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace Revenue_Recognition_System.Models;
+
+public class Category
+{
+    public int IdCategory { get; set; }
+    public string Name { get; set; }
+}
---
+++ Revenue_Recognition_System/Revenue_Recognition_System/Models/Software.cs
@@ -0,0 +1,9 @@
+namespace Revenue_Recognition_System.Models;
+
+public class Software
+{
+    public int IdSoftware { get; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Version { get; set; }
+}
